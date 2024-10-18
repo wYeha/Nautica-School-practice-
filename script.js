2a308878719fe7476dd677cae06212ca04126db8
wYeha <snovavzloman@mail.ru>
2024-10-18 18:58:44 +0300

    checkbox ebaniy done

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 const checkbox = document.getElementById('agreementCheckbox');
 const button = document.getElementById('submitForm');
+const fakeImage = document.getElementById('fakeImage');
 
 checkbox.addEventListener('change', function () {
 	button.disabled = !this.checked;
+
+	if (this.checked) {
+		fakeImage.style.opacity = 1;
+	} else {
+		fakeImage.style.opacity = 0;
+	}
 });
